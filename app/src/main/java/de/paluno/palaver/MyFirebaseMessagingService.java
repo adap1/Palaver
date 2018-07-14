7cc8827ea4ed59e5ce79c7f31616098b43295274
Andreas <skorpion9987@yahoo.de>
2018-07-14 19:11:11 +0200

    Push Notifications funktionieren teilweise

---
+++ app/src/main/java/de/paluno/palaver/MyFirebaseMessagingService.java
@@ -0,0 +1,34 @@
+package de.paluno.palaver;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+    private static final String TAG = "FCM Service";
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        // TODO: Handle FCM messages here.
+        // If the application is in the foreground handle both data and notification messages here.
+        // Also if you intend on generating your own notifications as a result of a received FCM
+        // message, here is where that should be initiated.
+        Log.d(TAG, "From: " + remoteMessage.getFrom());
+        Log.d(TAG, "Notification Message Body: " + remoteMessage.getNotification().getBody());
+
+    }
+
+    @Override
+    public void onNewToken(String s) {
+        super.onNewToken(s);
+        Log.e("onNewToken", s);
+
+        SharedPreferences prefs = getSharedPreferences("main", MODE_PRIVATE);
+        prefs.edit().putString("Token", s);
+    }
+
+
+}
